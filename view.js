a65d6d192306799ae3f8941132e66355c4c41ab0
btuaux <baptiste.tuaux@etudiant.univ-rennes.fr>
2025-10-15 12:09:40 +0200

    add view js comment

--- view.js
+++ view.js
@@ -1,3 +1,7 @@
+/* 
+  Contains the view part of the application (the canvas)
+  It uses the model defined in model.js
+  */
 Rectangle.prototype.paint = function (ctx) {
   ctx.strokeStyle = this.color;
   ctx.lineWidth = this.thickness;
@@ -7,7 +11,6 @@ Rectangle.prototype.paint = function (ctx) {
 Line.prototype.paint = function (ctx) {
   ctx.strokeStyle = this.color;
   ctx.lineWidth = this.thickness;
-  //TODO Manager color
   ctx.beginPath();
   ctx.moveTo(this.originX, this.originY);
   ctx.lineTo(this.endX, this.endY);
@@ -15,7 +18,6 @@ Line.prototype.paint = function (ctx) {
 };
 
 Drawing.prototype.paint = function (ctx) {
-  //console.log(this.getForms());
   ctx.fillStyle = '#F0F0F0'; // set canvas' background color
   ctx.fillRect(0, 0, canvas.width, canvas.height);
   this.shapes.forEach(element => element.paint(ctx));
