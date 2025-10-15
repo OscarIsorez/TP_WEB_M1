29d07e86bb53683ffaf9c3ac175198a6d71107b9
btuaux <baptiste.tuaux@etudiant.univ-rennes.fr>
2025-10-15 11:49:42 +0200

    Enhance Todo class documentation with detailed attribute descriptions

--- vue-project/src/modeles/Todo.ts
+++ vue-project/src/modeles/Todo.ts
@@ -1,4 +1,11 @@
 class Todo {
+  /* 
+    DataClass for a Todo item
+    Attributes:
+      - title: string - The title of the todo item
+      - state: string - The current state of the todo item (e.g., "pending", "done")
+      - date: Date - The date associated with the todo item
+  */
   title: string
   state: string
   date: Date
